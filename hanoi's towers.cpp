b390c6599dff2507e91522942036bbf7fba54a07
Andrei Constantinescu <92793459+Otelu@users.noreply.github.com>
2023-04-04 16:58:59 +0300

    Update hanoi's towers.cpp

--- hanoi's towers.cpp
+++ hanoi's towers.cpp
@@ -1,36 +1,42 @@
-#include<stdio.h>
+#include<iostream>
 
-int t1,t2,t3;
-int i,n,k;
+using namespace std;
+
+int t1, t2, t3;
+int i, n, k;
 
 void han(int n, int a, int b, int c)
 {
     if(n==1)
-        {printf("%d %d", a,b);
-        printf("\n");}
+    {
+        cout << a << " " << b << endl;
+    }
     else
-   {
-    han(n-1,t1,t3,t2);
-    printf("%d %d",t1,t2);
-    printf("\n");
-    han(n-1,t3,t2,t1);
+    {
+        han(n-1, t1, t3, t2);
+        cout << t1 << " " << t2 << endl;
+        han(n-1, t3, t2, t1);
     }
 }
+
 int main()
 {
-    printf("n=");
-    scanf("%d",&n);
+    cout << "n=";
+    cin >> n;
 
-    t1=1;
-    t2=2;
-    t3=3;
-    k=1;
-    for(i=1; i<=n; i++)
-        k=k*2;
-    printf("%d",k-1);
-    printf("\n");
+    t1 = 1;
+    t2 = 2;
+    t3 = 3;
+    k = 1;
 
-    han(n,t1,t3,t2);
+    for(i = 1; i <= n; i++)
+    {
+        k *= 2;
+    }
+
+    cout << k - 1 << endl;
+
+    han(n, t1, t3, t2);
 
     return 0;
 }
